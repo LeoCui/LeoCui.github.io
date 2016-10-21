1b4eb2a19d83bf1818a31ebf09140e7e9a5bcbf0
Leo <leocuiyiming@126.com>
2016-10-21 17:09:03 +0800

    Site updated: 2016-10-21 17:09:03

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@
     e.stopPropagation();
 
     var $this = $(this),
-      url = 'leocui.github.io',
+      url = $this.attr('data-url'),
       encodedUrl = encodeURIComponent(url),
       id = 'article-share-box-' + $this.attr('data-id'),
       offset = $this.offset();
